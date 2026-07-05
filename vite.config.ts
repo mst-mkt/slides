57b84e443bd85d839aa0855c9caf841154759bc4
mst-mkt <mail@keito.dev>
2026-07-06 06:14:06 +0900

    chore: deploy task

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,10 @@ export default defineConfig({
         command: 'vpr -F "./slides/*" --parallel export',
         cache: false,
       },
+      deploy: {
+        command: 'wrangler deploy',
+        cache: false,
+      },
       preview: {
         command: 'wrangler dev',
         cache: false,
